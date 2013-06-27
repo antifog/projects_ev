2a9e71669b247f1466a4c03c6511c6d89d9cefd1
antifog <superchunkie@gmail.com>
2013-06-26 23:55:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectsEv::Application.config.secret_token = 'f1385f7fd2a8cda41d7ac19210fa29da0ccd3b7668cf8b646346b324c93aa7cbb98ef22b8a6430da79cdbc878779ceda54de3487e3c0f9bc0d9a864208e0dfaf'
